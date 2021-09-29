d9005ac45165ef83739d11811ee34215191e919c
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2021-09-29 21:21:41 +0200

    Zent Cash Wallet - v3.4.9
    Webpack configs files

---
+++ configs/webpack.config.eslint.js
@@ -0,0 +1,6 @@
+// Copyright (C) 2021 Zent Cash Foundation
+//
+// Please see the included LICENSE file for more information.
+require("@babel/register");
+
+module.exports = require("./webpack.config.renderer.dev.babel").default;
